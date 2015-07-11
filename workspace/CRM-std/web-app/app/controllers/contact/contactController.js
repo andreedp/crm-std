7c589fc0d0e0a329e073bfe626f6fed14a5b7b6c
andree dwi prasetyo <and2e_7@yahoo.com>
2015-07-11 18:19:02 +0700

    Update CRM

--- workspace/CRM-std/web-app/app/controllers/contact/contactController.js
+++ workspace/CRM-std/web-app/app/controllers/contact/contactController.js
@@ -28,19 +28,19 @@ define(['controllers/controllers'],
 				$scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
 				$scope.format = $scope.formats[0];
 				
-				$scope.predicates = ['name', 'email', 'lastUpdated'];
+				$scope.predicates = ['name', 'email', 'telephone'];
 			    $scope.selectedPredicate = $scope.predicates[0];
 					  
 				$scope.contacts = Contact.query(function(data) {
 				    // success handler
-					console.log("Contact count ",  $scope.contacts.length); 
+					console.log("Contact count ",  $scope.contacts.length); 	
 					AlertService.clear();
-					AlertService.add('success', 'Query Success');
+					AlertService.add('success', 'Query Contact Success');
 				}, function(error) {
 				    // error handler     
 					console.log("Error");  
 					AlertService.clear();
-					AlertService.add('warning', 'Query Failed');
+					AlertService.add('warning', 'Query Contact Failed');
 				});
 			    $scope.orderProp = 'name';
 			    
@@ -49,9 +49,49 @@ define(['controllers/controllers'],
 			    }
 
 	}]);
+	
+	controllers.controller('contactViewController', ['$scope', '$routeParams', '$window', 'Contact', 'ContactService', 'AlertService',
+			                                          function($scope,  $routeParams, $window, Contact, ContactService, AlertService) {
+					
+					$scope.header = 'Contact Management';
+					$scope.title = 'Contact';
+					$scope.date = {startDate: null, endDate: null};
+					$scope.today = function() {
+					    $scope.dt = new Date();
+					  };
+					$scope.today();
+					$scope.dateOptions = {
+						    formatYear: 'yy',
+						    startingDay: 1
+					};
 
-	controllers.controller('contactCreateController', ['$log', '$scope', '$filter', '$location', '$routeParams', '$window', 'Contact', 'ContactService',
-			                                          function($log, $scope,  $filter, $location, $routeParams, $window, Contact, ContactService) {
+					$scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+					$scope.format = $scope.formats[0];
+					
+					$scope.predicates = ['name', 'email', 'lastUpdated'];
+				    $scope.selectedPredicate = $scope.predicates[0];
+						  
+					$scope.contacts = Contact.query(function(data) {
+					    // success handler
+						console.log("Contact count ",  $scope.contacts.length); 
+						AlertService.clear();
+						AlertService.add('success', 'Create Contact Success');
+					}, function(error) {
+					    // error handler     
+						console.log("Error");  
+						AlertService.clear();
+						AlertService.add('warning', 'Create Contact Failed');
+					});
+				    $scope.orderProp = 'name';
+				    
+				    $scope.OpenCourse = function(courseId) {
+				        $window.alert("Called " + courseId);
+				    }
+
+		}]);
+
+	controllers.controller('contactCreateController', ['$log', '$scope', '$filter', '$location', '$routeParams', '$window', 'Contact', 'ContactService', 'AlertService',
+			                                          function($log, $scope,  $filter, $location, $routeParams, $window, Contact, ContactService , AlertService) {
 					
 					$scope.header = 'Contact Management';
 					$scope.title = 'Contact';
@@ -90,17 +130,19 @@ define(['controllers/controllers'],
 				    $scope.save = function() {
 						$scope.contact.$save(function(contact, headers) {
 							$log.info('[ContactCreateController::save]Contact Save success: ' + angular.toJson(contact));										
+							AlertService.clear();
+							AlertService.add('success', 'Create Contact Success');
 							$location.path('contact/list/');
 						}, function(response) {
 							$log.info('[ContactCreateController::save]Contact Save failed: ' + angular.toJson(response));
 							if(response.data.errors)
 							{
-								$log.info('domain errors: ' + angular.toJson(response.data.errors));
-								//AlertService.addDomainErrors(response.data.errors);
+								$log.info('domain errors: ' + angular.toJson(response.data.errors));							
 							}
 							else
 							{
-								//AlertService.addAlert({type:'danger', message: ' ' + $scope.contact.name + ' '  + response.data});
+								AlertService.clear();
+								AlertService.add('warning', 'Create Contact Failed');
 							}
 						});
 					};
